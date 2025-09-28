be1237c5ce7eb09a6d3d1fe2ab67761f82320923
AvtandilJashi <j.avtandili1@gmail.com>
2025-09-28 11:19:12 +0400

    first commit

---
+++ Level 002/classwork/py.py
@@ -0,0 +1,87 @@
+#Classwork 1
+
+name1 = 'Goga'
+name2 = 'Avtandili'
+name3 = 'Nika'
+age1 = 20
+age2 = 15
+age3 = 16
+float1 = 1.80
+float2 = 1.78
+float3 = 1.82
+bool1 = False
+bool2 = True
+bool3 = True
+
+print(name1)
+print(name2)
+print(name3)
+print(age1)
+print(age2)
+print(age3)
+print(float1)
+print(float2)
+print(float3)
+print(bool1)
+print(bool2)
+print(bool3)
+
+
+
+#Classwork 2
+
+car = 'Bentley'
+model = 'Bentayaga'
+year = 2020
+engine = 3.5
+mile = 20000
+
+print(car)
+print(model)
+print(year)
+print(engine)
+print(mile)
+
+
+
+#Classwork 3
+
+num1 = 9
+num2 = 3
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+
+
+
+#Classwork 4
+
+num3 = 9.9
+num4 = 7.7
+
+result1 = num3 + num4
+result2 = num3 - num4
+result3 = num3 * num4
+result4 = num3 / num4
+result5 = num3 // num4
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+print(result5)
+
+
+
+#Classwork 5
+
+name = 'nika'
+name = 'luka'
+name = 'gio'
+name = 'goga'
+name = 'sandro'
+
+print(name)
---
+++ Level 002/homework/py.py
@@ -0,0 +1,134 @@
+#Homework 1
+
+#ჩვენ ვისწავლეთ 4 მონაცემთა ტიპი ესენია:
+
+#String - String არის ტექსტი რომელიც არის მოთავსებული ბრჭყალებში "". მაგალითად: "str" "int" "9".
+#Integer - Integer არის მთელი რიცხვები. მაგალითად: 9, 10, 11.
+#Float - Float არის იგივე ათწილადები. მაგალითად: 9.9, 20.8, 200.98.
+#Boolean - Boolean-ში მხოლოდ არის ორი მნიშვნელობა. True და False.
+
+
+
+#Homework 2
+
+name1 = 'mari'
+name2 = 'ato'
+name3 = 'goga'
+name4 = 'nika'
+name5 = 'sandro'
+age1 = 16
+age2 = 15
+age3 = 20
+age4 = 18
+age5 = 14
+height1 = 1.65
+height2 = 1.78
+height3 = 1.80
+height4 = 1.85
+height5 = 1.70
+is_student1 = True
+is_student2 = True
+is_student3 = False
+is_student4 = False
+is_student5 = True
+
+print(name1)
+print(name2)
+print(name3)
+print(name4)
+print(name5)
+print(age1)
+print(age2)
+print(age3)
+print(age4)
+print(age5)
+print(height1)
+print(height2)
+print(height3)
+print(height4)
+print(height5)
+print(is_student1)
+print(is_student2)
+print(is_student3)
+print(is_student4)
+print(is_student5)
+
+
+
+#Homework 3
+
+num1 = 9
+num2 = 13
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+
+
+
+#Homework 4
+
+num3 = 80.9
+num4 = 78.7
+
+result1 = num3 + num4
+result2 = num3 - num4
+result3 = num3 * num4
+result4 = num3 / num4
+result5 = num3 // num4
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+print(result5)
+
+
+
+#Homework 5
+
+user_name = 'gela'
+last_name = 'barkalaia'
+model_name = 'bentayaga'
+introduce_year = 2022
+car_name = 'bentley'
+
+print(user_name)
+print(last_name)
+print(model_name)
+print(introduce_year)
+print(car_name)
+
+
+
+#Homework 6
+
+
+name = 'tekla'
+name = 'zviadi'
+name = 'zauri'
+name = 'guga'
+name = 'lasha'
+
+print(name)
+
+#ჩვენ ახლა გამოგვიტანს ტერმინალში lasha-ს და რატომ ხდება ასე?
+#ასე იმიტომ ხდება რომ პითონი აიგნორებს აბსოლიტურად ყველა ცვლადს გარდა იმისა რომელიც ბოლოს წერია და შესაბამისად გამოიტანს lasha-ს რადგან ბოლო წერია
+
+
+
+#Homework 7
+
+NAme = 'cici'
+Name = 'luiza'
+nAme = 'luka'
+naMe = 'ana'
+namE = 'lizi'
+
+print(NAme)
+print(Name)
+print(nAme)
+print(naMe)
+print(namE)
