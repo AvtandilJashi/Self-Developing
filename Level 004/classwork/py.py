28dde9bf3f521cb2479965e369fbbb7bd0218eaf
AvtandilJashi <j.avtandili1@gmail.com>
2025-10-05 04:44:25 +0400

    4 commit

---
+++ Level 004/classwork/py.py
@@ -0,0 +1,56 @@
+#Classwork 1
+
+number = int(input('Enter number: '))
+Number = int(input('Enter number: '))
+
+print(number + Number)
+print(number - Number)
+print(number * Number)
+print(number / Number)
+print(number // Number)
+
+
+
+#Classwork 2
+
+name = input('Enter name: ')
+nUmber = int(input('Enter number: '))
+
+print(name * nUmber)
+
+
+
+#Classwork 3
+
+NUmber = int(input('Enter number: '))
+NuMber = int(input('Enter number: '))
+
+print(NUmber > NuMber)
+print(NUmber < NuMber)
+print(NUmber == NuMber)
+print(NUmber >= NuMber)
+print(NUmber <= NuMber)
+
+
+
+
+#Classwork 4
+
+int = 9
+float = 9.0
+bool = True
+str = '9'
+
+print(type(int))
+print(type(float))
+print(type(bool))
+print(type(str))
+
+
+
+#Classwork 5
+
+NAMe = 'me'
+namE = 'me'
+
+print(NAMe == namE)
---
+++ Level 004/homework/py.py
@@ -0,0 +1,163 @@
+#Homework 1
+
+name = '15'
+
+print(int(name) + 5)
+
+
+
+#Homework 2
+
+enter_number = float(input('Enter number: '))
+
+print(enter_number * 2)
+
+
+
+#Homework 3
+
+user_name1 = input('Enter number: ')
+user_name2 = input('Enter number: ')
+
+print(int(user_name1) + int(user_name2))
+print(int(user_name1) * int(user_name2))
+
+
+
+#Homework 4
+
+user_name3 = input('Enter your name: ')
+user_age = int(input('Enter your age: '))
+
+print(f'Hello {user_name3}, you are {user_age} old.')
+
+
+
+#Homework 5
+
+str = input('Enter letter: ')
+int = int(input('Enter number: '))
+
+print(str * int)
+
+
+
+#Homework 6
+
+value = input('Enter value: ')
+
+print(type(value))
+
+
+
+#Homework 7
+
+int2 = int(input('Enter number: '))
+
+print(type(str(int2)))
+
+
+
+#Homework 8
+
+Float = '25.5'
+
+print(type(Float))
+print(type(float(Float)))
+
+
+
+#Homework 9
+
+example1 = 'hello'
+example2 = 123
+example3 = 12.5
+
+print(type(example1))
+print(type(example2))
+print(type(example3))
+
+
+
+#Homework 10
+
+var1 = input('Enter str: ')
+var2 = input('Enter int: ')
+var3 = input('Enter float: ')
+
+print(type(var1))
+print(type(int(var2)))
+print(type(float(var3)))
+
+
+
+# Homework 11
+
+first_variable = input('Enter your name: ')
+second_variable = int(input('Enter your age: '))
+third_variable = float(input('Enter your height: '))
+
+my_name = first_variable
+my_age = second_variable
+my_height = third_variable
+
+print(f'Hello my name is {my_name}, I am {my_age} old and my height is {my_height}.')
+
+
+
+#Homework 12
+
+first_decimal = float(input('Enter decimal: '))
+second_decimal = float(input('Enter decimal: '))
+
+print(first_decimal + second_decimal)
+print(first_decimal - second_decimal)
+print(first_decimal * second_decimal)
+print(first_decimal / second_decimal)
+print(first_decimal // second_decimal)
+
+
+
+#Homework 13
+
+#<, >, ==, >=, <=.
+
+#ესენი სულ ყველა პასუხად აბრუნებენ True ან False-ს.
+
+
+
+#Homework 14
+
+first_num = int(input('Enter number: '))
+second_num = int(input('Enter number: '))
+
+print(first_num < second_num)
+print(first_num > second_num)
+print(first_num == second_num)
+print(first_num >= second_num)
+print(first_num <= second_num)
+
+
+
+#Homework 15
+
+first_str = input('Enter str: ')
+second_str = input('Enter str: ') 
+
+print(first_str == second_str)
+
+
+
+#Homework 16
+
+number1 = 9
+number2 = 13
+sum_numbers = number1 + number2
+number3 = 21
+
+print(sum_numbers < number3)
+print(sum_numbers > number3)
+print(sum_numbers == number3)
+print(sum_numbers >= number3)
+print(sum_numbers <= number3)
+
