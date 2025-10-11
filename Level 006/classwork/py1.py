391b7d1230820817b38e9804cf74bd57f76acf73
AvtandilJashi <j.avtandili1@gmail.com>
2025-10-11 21:38:08 +0400

    commit

--- Level 006/classwork/py1.py
+++ Level 006/classwork/py1.py
@@ -1,2 +1,57 @@
 #Classwork 1
 
+# True or True --> True
+# True or False --> True
+# False or True --> True
+# False or False --> False
+
+# True and True --> True
+# True and False --> False
+# False and True --> False
+# False and False --> False
+
+
+
+#Classwork 2
+
+# 5 != 3 and 3 == 3 --> True
+# 5 >= 5 and 194 < 564  --> True
+# 10 == 10 and 50 >= 45  --> True
+# 45 <= 45 and 10 != 5 --> True 
+# 456 > 76 and 312 < 400 --> True
+# 50 <= 50 and "gio" != "gio" --> False
+
+# 5 < 60 or 10 != 90 --> True
+# 50 == 10 or 50 < 40 --> False
+# "giorgi" == "giorgi or "lasha" != "goga" --> True
+# 10 < 4 or 90 >= 90 --> True
+# 13 <= 30 or 34 >= 4 --> True
+# 50 == 50 or 3 != 3 --> True
+
+
+
+#Classwork 3
+
+for i in range(15):
+    print('Avtandili')
+
+
+
+#Classwork 4
+
+for i in range(1, 20):
+    print(i)
+
+
+
+#Classwork 5
+
+for i in range(100):
+    print('Avtandil Jashi')
+
+
+
+#Classwork 6
+
+for i in range(200):
+    print(i)
---
+++ Level 006/homework/py.py
@@ -0,0 +1,144 @@
+#Homework 1
+
+num1 = int(input('Enter number: '))
+
+print(num1 > 10 and num1 < 50)
+
+
+
+#Homework 2
+
+num2 = int(input('Enter number: '))
+
+print(num2 < 0 or num2 == 0)
+
+
+
+#Homework 3
+
+#იგივე იყო რაც Homework 1
+
+
+
+#Homework 4
+
+#იგივე იყო რაც Homework 2
+
+
+
+#Homework 5
+
+age1 = int(input('Enter your age: '))
+
+print(age1 >= 18 and age1 < 65)
+
+
+
+#Homework 6
+
+num6 = int(input('Enter number: '))
+
+print(num6 < 0 or num6 > 100)
+
+
+
+#Homework 7
+
+num7 = int(input('Enter number: '))
+
+print(num7 > 0 and num7 % 2 == 0)
+
+
+
+#Homework 8
+
+num8 = int(input('Enter number: '))
+
+print(num8 % 3 == 0 or num8 % 5 == 0)
+
+
+
+#Homework 9
+
+temperature = int(input('Enter temperature: '))
+
+print(temperature > 0 and temperature < 35)
+
+
+
+#Homework 10
+
+num10 = int(input('Enter number: '))
+
+print(num10 < 0 or num10 == 10)
+
+
+
+#Homework 11
+
+age2 = int(input('Enter your age: '))
+
+print(age2 > 13 and age2 < 19)
+
+
+
+#Homework 12
+
+num12 = int(input('Enter number: '))
+
+print(num12 % 2 == 0 or num12 < 0)
+
+
+
+#Homework 13
+
+for i in range(1, 10):
+    print(i)
+
+
+
+#Homework 14
+
+for i in range(2, 20, 2):
+    print(i)
+
+
+
+#Homework 15
+
+for i in range(1, 15, 2):
+    print(i)
+
+
+
+#Homework 16
+
+for i in range(100, 50, -5):
+    print(i)
+
+
+
+#Homework 17
+
+variable = 'Python'
+
+for i in range(25):
+    print(variable)
+
+
+
+#Homework 18
+
+name_lastname = 'Avtandil Jashi'
+
+for i in range(100):
+    print(name_lastname)
+
+
+
+#Homework 19
+
+variable2 = 'ხოდა ეგრე ბიჭო'
+
+for i in range(60):
+    print(variable2)
